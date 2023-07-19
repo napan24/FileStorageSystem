8450a8a4b4b4c0ff8a0076f322d106b5b08c4713
Neil Sagar Sahu <neilsagar55555@gmail.com>
2023-07-19 20:47:46 +0530

    added .env file and added database link there

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 const cors=require('cors');
@@ -10,7 +12,7 @@ const signupmodel=require('./models/signupdetails');
 const SavedFiles = require('./models/SavedFiles');
 const SavedForm=require('./models/SaveForm');
 const ConfirmForm=require('./models/ConfirmForm');
-const db = "mongodb+srv://napan:1234@cluster0.1iy39cq.mongodb.net/?retryWrites=true&w=majority";
+const db = process.env.DB_LINK;
 
 app.post("/",(req,res)=>{
     const {email,password}=req.body;
